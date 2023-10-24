f49972212fb60467d9d3d466ba08d4fb3d700ddc
Samiir Ahmed <samiir.mahamed6@gmail.com>
2023-10-24 13:26:40 +0200

    GameboardClasSecondeCommit

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,14 +1,22 @@
 import java.util.ArrayList;
 
-public class GameBoard {
+public class GameBoard  {
     private int boardSize;
-     private ArrayList<ArrayList<Cell>> gameBoard;
+    private ArrayList<ArrayList<Cell>> gameBoard;
     private int bombCount;
 
     public GameBoard(int boardSize,int bombCount) {
         this.boardSize = boardSize;
         this.bombCount = bombCount;
         this.gameBoard = new ArrayList<>();
+        for (int i = 0; i < boardSize; i++) {
+            ArrayList<Cell> innerBox = new ArrayList<>();
+            for (int j = 0; j < boardSize; j++) {
+                innerBox.add(new Cell());
+            }
+            gameBoard.add(innerBox);
+        }
+
     }
 
     public int getBoardSize() {
