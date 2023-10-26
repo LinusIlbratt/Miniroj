8ad6cfbb6fa916f200120b3a6b43be8d8a5ae5e8
mouner alkrdi <mouneralkrdi@alMacBook-Pro-alkhas-bmouner.local>
2023-10-26 11:21:37 +0200

    player innuti Game

--- src/Game.java
+++ src/Game.java
@@ -6,11 +6,14 @@ public class Game {
     private Scanner scan;
     private Player player;
     private String gameState; // ska utvecklas senare med metoden startGame() med olika souts och vid running (printBoard();), won (isWon) eller lost (revealAll)
+    private String playerName; // Namnet på spelare.
 
     public Game() {
         scan = new Scanner(System.in);
         gameBoard = new GameBoard(8, 10); // 8x8 with 10 bombs
-
+        System.out.println("inter your naem ");
+        String playerName = scan.nextLine();
+        this.player = new Player(playerName);
 
     }
 }
