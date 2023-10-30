7e9143958a04bd073c83b9b8a1d7ffe59dab0a8d
Linus Ilbratt <linusilbratt@gmail.com>
2023-10-30 19:49:34 +0100

    connecting the game timer to the game

--- src/GameTimer.java
+++ src/GameTimer.java
@@ -14,11 +14,9 @@ public class GameTimer {
     public void stopTime(){
         endTime = System.currentTimeMillis();
     }
-
     public double totalTimeInSeconds() {
         return (endTime - startTime) / 1000.0;
     }
-
     public String convertSecondsToHMS(double totalSeconds){
         int totalS = (int) totalSeconds;
         int hours = totalS / 3600;
@@ -28,7 +26,6 @@ public class GameTimer {
 
         return String.format("%02d:%02d:%02d", hours, minutes, seconds);
     }
-
     public String elapsedTime() {
         double elapseTimeInSeconds = totalTimeInSeconds();
         return convertSecondsToHMS(elapseTimeInSeconds);
