23570fa957d5f337216ff994565c0e5690ea6006
Natalie Samaan <nataliesamaan@MacBook-Air-som-tillhor-Natalie.local>
2023-10-31 18:28:11 +0100

    fixing menu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,59 @@
+import java.util.Scanner;
+
 public class Main {
+
+    static Scanner s;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        int num;
+        do {
+            System.out.println("Welcome to Game Menu\n"
+                    + "1.Play\n"
+                    + "2.Exit\n");
+            s = new Scanner(System.in);
+            num = s.nextInt();
+
+            switch (num) {
+                case 1:
+                    play();
+                    break;
+                case 2:
+                    exit();
+                    break;
+                default:
+                    break;
+            }
+        }
+        while (num != 0);
+    }
+
+    private static void play() {
+        int num;
+        do {
+            System.out.println("Which level do you want to play?");
+            System.out.println("1. Easy");
+            System.out.println("2. Difficult");
+            s = new Scanner(System.in);
+            num = s.nextInt();
+
+            switch (num) {
+                case 1:
+                    System.out.println("You choosed easy");
+                    Game miniroj = new Game();
+                    miniroj.startGame();
+                    break;
+                case 2:
+                    System.out.println("You choosed difficult");
+                    break;
+                default:  System.out.println("Wrong input, try again");
+                    break;
+            }
+        }
+        while (num != 0);
+
+    }
+    private static void exit() {
     }
 }
